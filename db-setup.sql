79b5d870f15ee1a52114c9833874c4a435f40e60
mikedilulo <mikedilulo2019@gmail.com>
2020-03-17 00:52:25 -0600

    Bugs Table setup for creation upon connection

--- db-setup.sql
+++ db-setup.sql
@@ -1,57 +1,26 @@
--- USE keepr13;
+-- USE databaseNAME;
 
--- CREATE TABLE vaults (
+-- CREATE TABLE bugs (
 --     id int NOT NULL AUTO_INCREMENT,
---     name VARCHAR(255) NOT NULL,
---     description VARCHAR(255) NOT NULL,
 --     userId VARCHAR(255),
---     INDEX userId (userId),  
---     PRIMARY KEY (id)
--- );
-
--- CREATE TABLE keeps (
---     id int NOT NULL AUTO_INCREMENT,
---     name VARCHAR(255) NOT NULL,
+--     title VARCHAR(255) NOT NULL,
+--      subject VARCHAR(255) NOT NULL,
 --     description VARCHAR(255) NOT NULL,
---     userId VARCHAR(255),
---     img VARCHAR(255),
---     isPrivate TINYINT,
---     views INT DEFAULT 0,
---     shares INT DEFAULT 0,
---     keeps INT DEFAULT 0,
+--     reportedBy VARCHAR(255) NOT NULL,
+--     isClosed TINYINT,
+--     daysOpen INT,
+--     lastModified DATETIME,
+--     bugCreated DATETIME,
+--     bugClosed DATETIME,
 --     INDEX userId (userId),
 --     PRIMARY KEY (id)
 -- );
 
--- CREATE TABLE vaultkeeps (
---     id int NOT NULL AUTO_INCREMENT,
---     vaultId int NOT NULL,
---     keepId int NOT NULL,
---     userId VARCHAR(255) NOT NULL,
+-- -- USE THIS TO SELECT ALL THE DATA IN THE TABLE
+-- SELECT * FROM bugs;
 
---     PRIMARY KEY (id),
---     INDEX (vaultId, keepId),
---     INDEX (userId),
-
---     FOREIGN KEY (vaultId)
---         REFERENCES vaults(id)
---         ON DELETE CASCADE,
-
---     FOREIGN KEY (keepId)
---         REFERENCES keeps(id)
---         ON DELETE CASCADE
--- )
-
-
--- -- USE THIS LINE FOR GET KEEPS BY VAULTID
--- SELECT k.* FROM vaultkeeps vk
--- INNER JOIN keeps k ON k.id = vk.keepId 
--- WHERE (vaultId = @vaultId AND vk.userId = @userId) 
-
-
+-- -- USE THIS TO WIPE DATA INSIDE OF TABLE
+-- TRUNCATE TABLE bugs;
 
 -- -- USE THIS TO CLEAN OUT YOUR DATABASE
--- DROP TABLE IF EXISTS vaultkeeps;
--- DROP TABLE IF EXISTS vaults;
--- DROP TABLE IF EXISTS keeps;
--- DROP TABLE IF EXISTS users;
+-- DROP TABLE IF EXISTS bugs;
