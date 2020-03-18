123cd33ed9a7d3b5b213fd3ea9dc343a0454ed83
mikedilulo <mikedilulo2019@gmail.com>
2020-03-18 15:56:19 -0600

    Added Notes Table

--- db-setup.sql
+++ db-setup.sql
@@ -16,11 +16,24 @@
 --     PRIMARY KEY (id)
 -- );
 
+-- CREATE TABLE notes (
+--   id int NOT NULL AUTO_INCREMENT,
+--   userId VARCHAR(255),
+--   title VARCHAR(255) NOT NULL,
+--   description VARCHAR(255) NOT NULL,
+--   noteCreatedBy VARCHAR(255) NOT NULL,
+--   INDEX userId(userId),
+--    PRIMARY KEY (id)
+-- )
+
 -- -- USE THIS TO SELECT ALL THE DATA IN THE TABLE
 -- SELECT * FROM bugs;
+-- SELECT * FROM notes;
 
 -- -- USE THIS TO WIPE DATA INSIDE OF TABLE
 -- TRUNCATE TABLE bugs;
+-- TRUNCATE TABLE notes;
 
 -- -- USE THIS TO CLEAN OUT YOUR DATABASE
 -- DROP TABLE IF EXISTS bugs;
+-- DROP TABLE IF EXISTS notes;
