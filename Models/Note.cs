dea58a634c59f8fc11bf4b786dc7fe195a2623d3
mikedilulo <mikedilulo2019@gmail.com>
2020-03-18 13:27:39 -0600

    Note Model completed

--- Models/Note.cs
+++ Models/Note.cs
@@ -1,4 +1,10 @@
 namespace Keepr.Models
 {
-
+  public class Note
+  {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string NoteCreatedBy { get; set; }
+  }
 }
