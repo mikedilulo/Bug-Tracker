73eb157abf9303fe9ad9b8720be46cdcf5c98011
mikedilulo <mikedilulo2019@gmail.com>
2020-03-18 13:33:38 -0600

    Notes Repository complete prior to SQL commands

--- Repositories/NotesRepository.cs
+++ Repositories/NotesRepository.cs
@@ -1,7 +1,13 @@
+using System.Data;
+
 namespace Keepr.Repositories
 {
   public class NotesRepository
   {
-
+    private readonly IDbConnection _db;
+    public NotesRepository(IDbConnection db)
+    {
+      _db = db;
+    }
   }
 }
